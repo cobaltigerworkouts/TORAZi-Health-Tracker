92a77d64341cc35ab2ba0753c4659394a3939dda
cobaltigerworkouts <cobaltiger.workouts@gmail.com>
2026-08-28 16:54:29 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-function App() {
-  return (
-    <div style={{ padding: '20px', fontFamily: 'sans-serif', textAlign: 'center' }}>
-      <h1>Health Tracker</h1>
-      <p>アプリが正常に起動しました！</p>
-    </div>
-  )
-}
+import App from './health-tracker.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
